0809fdeb094e2212af5320ab035cce644dff18eb
shiori-furuya <f.sio232@gmail.com>
2020-11-08 18:52:33 +0900

    kadai1

---
+++ src/kadai1/helloworld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package kadai1;
+
+/**
+ * @author furuyashiori
+ *
+ */
+public class helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("HelloWorld");
+
+	}
+
+}
